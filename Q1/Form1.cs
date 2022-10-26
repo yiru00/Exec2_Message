9906688ef97f72ac7a1bbdafe9aff8a112675e13
yiru <eva890706@gmail.com>
2022-10-26 10:53:00 +0800

    加入專案檔案。

---
+++ Q1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			label1.Text = string.Empty;
+		}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			string result=$"{DateTime.Now:HH:mm:ss}\n\aHI";
+			label1.Text = result;
+		}
+    }
+}
---
+++ Q2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			string result = $"目前時間是: {DateTime.Now:yyyy/MM/dd HH:mm:ss}";
+			MessageBox.Show(result);
+		}
+    }
+}
